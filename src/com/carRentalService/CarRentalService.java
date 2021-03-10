564f499fd0e5d82da85966de3bc1b97d181d48a8
AlidaSchefers <alidadschefers@gmail.com>
2021-03-09 22:50:28 -0500

    added if-else to test for valid and invalid inputs

--- src/com/carRentalService/CarRentalService.java
+++ src/com/carRentalService/CarRentalService.java
@@ -10,12 +10,19 @@ public class CarRentalService {
         Scanner scanner = new Scanner(System.in);
 
         while(true) {
+            if(availableCars.length == 0)
+                break;
             System.out.println("Available Cars:");
             for (int i = 0; i < availableCars.length; i++) {
                 System.out.println((i+1)+") "+availableCars[i]);
             }
             System.out.print("Selection: ");
             byte selection = scanner.nextByte();
+            if(selection >= 1 && selection <= availableCars.length) { //valid selection
+
+            }else{ //invalid selection
+
+            }
         }
 
     }
