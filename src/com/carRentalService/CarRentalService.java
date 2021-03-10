2806379bc17afd04aa0ab0a7c1816e2366b59778
AlidaSchefers <alidadschefers@gmail.com>
2021-03-10 18:08:08 -0500

    can handle inputs that are not type byte

--- src/com/carRentalService/CarRentalService.java
+++ src/com/carRentalService/CarRentalService.java
@@ -10,9 +10,8 @@ public class CarRentalService {
         availableCars.add("Chevy");
         availableCars.add("Toyota");
 
-        Scanner scanner = new Scanner(System.in);
-
-        while(true) {
+        while(true) { //handling non-byte inputs
+            Scanner scanner = new Scanner(System.in);
             if(availableCars.size() == 0)
                 break;
             System.out.println("Available Cars:");
@@ -20,36 +19,18 @@ public class CarRentalService {
                 System.out.println((i+1)+") "+availableCars.get(i));
             }
             System.out.print("Selection: ");
-            byte selection = scanner.nextByte();
-            if(selection >= 1 && selection <= availableCars.size()) { //valid selection
-                availableCars.remove(selection-1);
-            }else{ //invalid byte input. if there's a non-byte input, get InputMismatchException and program ends
-                System.out.println("Invalid selection. Please try again.");
+
+            try{
+                byte selection = scanner.nextByte();
+                if (selection >= 1 && selection <= availableCars.size()) { //valid selection
+                    availableCars.remove(selection - 1);
+                } else { //invalid selection
+                    System.out.println("Car selection invalid. Please try again.");
+                }
+            }catch (Exception e){ //if an invalid input is given (e.g. "w"), then there's an infinite loop that displays the available cars and "selection: invalid input"
+                System.out.println("invalid input");
             }
         }
-
         System.out.println("No cars are currently available.");
-
-//        while(true) { //handling non-byte inputs
-//            if(availableCars.size() == 0)
-//                break;
-//            System.out.println("Available Cars:");
-//            for (int i = 0; i < availableCars.size(); i++) {
-//                System.out.println((i+1)+") "+availableCars.get(i));
-//            }
-//            System.out.print("Selection: ");
-//
-//            try{
-//                byte selection = scanner.nextByte();
-//                if (selection >= 1 && selection <= availableCars.size()) { //valid selection
-//                    availableCars.remove(selection - 1);
-//                } else { //invalid selection
-//                    System.out.println("Car selection invalid. Please try again.");
-//                }
-//            }catch (Exception e){ //if an invalid input is given (e.g. "w"), then there's an infinite loop that displays the available cars and "selection: invalid input"
-//                System.out.println("invalid input");
-//            }
-//        }
-
     }
 }
