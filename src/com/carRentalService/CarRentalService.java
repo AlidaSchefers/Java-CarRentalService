f54957e1932ea9083c9d4de56c38667b366a4263
AlidaSchefers <alidadschefers@gmail.com>
2021-03-09 21:39:21 -0500

    initial commit

---
+++ src/com/carRentalService/CarRentalService.java
@@ -0,0 +1,22 @@
+package com.carRentalService;
+
+import java.util.Scanner;
+
+public class CarRentalService {
+    public static void main(String[] args) {
+        String[] availableCars = {"Honda", "Chevy", "Toyota"};
+        String[] rentedCars = {};
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Available Cars:");
+            for (int i = 0; i < availableCars.length; i++) {
+                System.out.println((i+1)+") "+availableCars[i]);
+            }
+            System.out.print("Selection: ");
+            byte selection = scanner.nextByte();
+        }
+
+    }
+}
