07abe38492d2d8d6a825b8f9e8ec19111bcbeb2e
AlidaSchefers <alidadschefers@gmail.com>
2021-03-10 16:44:49 -0500

    added comment of potential handling of non-byte inputs

--- src/com/carRentalService/CarRentalService.java
+++ src/com/carRentalService/CarRentalService.java
@@ -23,10 +23,33 @@ public class CarRentalService {
             byte selection = scanner.nextByte();
             if(selection >= 1 && selection <= availableCars.size()) { //valid selection
                 availableCars.remove(selection-1);
-            }else{ //invalid selection
+            }else{ //invalid byte input. if there's a non-byte input, get InputMismatchException and program ends
                 System.out.println("Invalid selection. Please try again.");
             }
         }
+
         System.out.println("No cars are currently available.");
+
+//        while(true) { //handling non-byte inputs
+//            if(availableCars.size() == 0)
+//                break;
+//            System.out.println("Available Cars:");
+//            for (int i = 0; i < availableCars.size(); i++) {
+//                System.out.println((i+1)+") "+availableCars.get(i));
+//            }
+//            System.out.print("Selection: ");
+//
+//            try{
+//                byte selection = scanner.nextByte();
+//                if (selection >= 1 && selection <= availableCars.size()) { //valid selection
+//                    availableCars.remove(selection - 1);
+//                } else { //invalid selection
+//                    System.out.println("Car selection invalid. Please try again.");
+//                }
+//            }catch (Exception e){ //if an invalid input is given (e.g. "w"), then there's an infinite loop that displays the available cars and "selection: invalid input"
+//                System.out.println("invalid input");
+//            }
+//        }
+
     }
 }
