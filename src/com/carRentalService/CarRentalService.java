e6431acf0bcbc1aca137404b2dd87bc93124bfe8
AlidaSchefers <alidadschefers@gmail.com>
2021-03-10 15:59:07 -0500

    completing without OOP

--- src/com/carRentalService/CarRentalService.java
+++ src/com/carRentalService/CarRentalService.java
@@ -1,29 +1,32 @@
 package com.carRentalService;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CarRentalService {
     public static void main(String[] args) {
-        String[] availableCars = {"Honda", "Chevy", "Toyota"};
-        String[] rentedCars = {};
+        ArrayList<String> availableCars = new ArrayList<String>();
+        availableCars.add("Honda");
+        availableCars.add("Chevy");
+        availableCars.add("Toyota");
 
         Scanner scanner = new Scanner(System.in);
 
         while(true) {
-            if(availableCars.length == 0)
+            if(availableCars.size() == 0)
                 break;
             System.out.println("Available Cars:");
-            for (int i = 0; i < availableCars.length; i++) {
-                System.out.println((i+1)+") "+availableCars[i]);
+            for (int i = 0; i < availableCars.size(); i++) {
+                System.out.println((i+1)+") "+availableCars.get(i));
             }
             System.out.print("Selection: ");
             byte selection = scanner.nextByte();
-            if(selection >= 1 && selection <= availableCars.length) { //valid selection
-
+            if(selection >= 1 && selection <= availableCars.size()) { //valid selection
+                availableCars.remove(selection-1);
             }else{ //invalid selection
-
+                System.out.println("Invalid selection. Please try again.");
             }
         }
-
+        System.out.println("No cars are currently available.");
     }
 }
