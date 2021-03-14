6fedfaf19de9fba88a40deec5a12166b6868e397
AlidaSchefers <alidadschefers@gmail.com>
2021-03-14 17:23:54 -0400

    currently only runs the Rent sequence

--- src/com/carRentalService/CarRentalServiceMain.java
+++ src/com/carRentalService/CarRentalServiceMain.java
@@ -5,15 +5,16 @@ import java.util.ArrayList;
 public class CarRentalServiceMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Java Car Rentals!");
+        RentSequence.runRentSequence(); //the RENT loop
         //start the loop! while(true).
         //then can enter one of two loops: rent or return. when want to return to main stage, use break to leave the loop!
 
-        ArrayList<String> availableCars = AvailableCars.getAvailableCars();
-        while(availableCars.size() != 0) {
-            AvailabilityReport.printAvailableCars(availableCars);
-            Console.readCarSelection(availableCars);
-        }
-        System.out.print("No cars are currently available.");
+//        ArrayList<String> availableCars = AvailableCars.getAvailableCars();
+//        while(availableCars.size() != 0) {
+//            AvailabilityReport.printAvailableCarsStr(availableCars);
+//            Console.readCarSelection(availableCars);
+//        }
+//        System.out.print("No cars are currently available.");
     }
 
 }
