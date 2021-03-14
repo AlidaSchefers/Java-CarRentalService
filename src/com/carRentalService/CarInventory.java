c2afdd6ec3bb868dfc5d21b614b25a076b10bf49
AlidaSchefers <alidadschefers@gmail.com>
2021-03-13 22:19:29 -0500

    instantiated car objects

--- src/com/carRentalService/CarInventory.java
+++ src/com/carRentalService/CarInventory.java
@@ -1,2 +1,29 @@
-package com.carRentalService;public class CarInventory {
+package com.carRentalService;
+
+import java.util.ArrayList;
+
+public class CarInventory {
+//    private static String[] carModelsArr = new String[]{"Honda", "Chevy", "Toyota", "Hyundai", "BMW"};
+
+    public ArrayList<Object> giveCarInventory() {
+        ArrayList<Object> carInventoryArr = new ArrayList<Object>();
+
+        Car car001 = new Car("Honda", true);
+        carInventoryArr.add(car001);
+
+        Car car002 = new Car("Chevy", true);
+        carInventoryArr.add(car002);
+
+        Car car003 = new Car("Toyota", true);
+        carInventoryArr.add(car003);
+
+        Car car004 = new Car("Hyundai", true);
+        carInventoryArr.add(car004);
+
+        Car car005 = new Car("BMW", true);
+        carInventoryArr.add(car005);
+
+        return carInventoryArr;
+
+    }
 }
