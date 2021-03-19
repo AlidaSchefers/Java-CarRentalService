d593843fdd966e4a7209e70ddfe89bf281b6d923
AlidaSchefers <alidadschefers@gmail.com>
2021-03-18 22:52:23 -0400

    added a parameter to the method

--- src/com/carRentalService/RentSequence.java
+++ src/com/carRentalService/RentSequence.java
@@ -4,9 +4,8 @@ import java.util.ArrayList;
 
 public class RentSequence {
     //loop starts here. break when want to return to home screen.
-    public static void runRentSequence() {
+    public static void runRentSequence(ArrayList<Car> carInventoryArr) {
         while(true) {
-            ArrayList<Car> carInventoryArr = CarInventory.getCarInventory();
             ArrayList<Car> availableCarObjs = SplitCars.giveCarsArrByAvailability(carInventoryArr, true);
             Console.readRentCarSelectionWCarArr(availableCarObjs);
             Console.readNewRentalName("What is the name you would like to use for your rental? ", Console.selectedCarByte, availableCarObjs);
