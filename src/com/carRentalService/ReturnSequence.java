6e7407c85918fe4913db5668838979fc652d38c0
AlidaSchefers <alidadschefers@gmail.com>
2021-03-18 22:56:19 -0400

    added runReturnSequence method

--- src/com/carRentalService/ReturnSequence.java
+++ src/com/carRentalService/ReturnSequence.java
@@ -1,4 +1,17 @@
 package com.carRentalService;
 
+import java.util.ArrayList;
+
 public class ReturnSequence {
+    static public void runReturnSequence(ArrayList<Car> carInventoryArr){
+        while(true){
+            ArrayList<Car> rentedCarObjs = SplitCars.giveCarsArrByAvailability(carInventoryArr, false);
+            Console.readReturnCarSelection(rentedCarObjs);
+//            String nameConfirmationPrompt = "Enter the name you used to rent the "+rentedCarObjs.get(Console.selectedCarByte).getMake()+".")
+            Console.readRenterNameReturnConfirmation(("Enter  the name you used to rent the "+rentedCarObjs.get(Console.selectedCarByte-1).getMake()+".\nEnter Name: "),
+                    Console.selectedCarByte,
+                    rentedCarObjs);
+            break;
+        }
+    }
 }
