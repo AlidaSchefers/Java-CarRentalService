82682e79c10bdc928aa5e3fe8fb95977c2382141
AlidaSchefers <alidadschefers@gmail.com>
2021-03-18 22:50:49 -0400

    fixed double return input bug

--- src/com/carRentalService/HomeScreen.java
+++ src/com/carRentalService/HomeScreen.java
@@ -3,27 +3,57 @@ package com.carRentalService;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//public class HomeScreen {
+//    static public String readPathSelection() {
+//        Scanner scanner = new Scanner(System.in);
+//        while(true) {
+//            System.out.print("Selection: ");
+//            String pathSelection = scanner.nextLine();
+//            if (pathSelection.toLowerCase().matches("rent")) {
+//                return "rent";
+//            }else if (pathSelection.toLowerCase().matches("return")) {
+//                return "return";
+//            } else {
+//                System.out.println("Invalid input. Please try again.");
+//            }
+//        }
+////       String pathSelection;
+////        do {
+////            System.out.print("Selection: ");
+////            pathSelection = scanner.nextLine();
+////            if (pathSelection.toLowerCase().matches("rent")) {
+//////                return "rent";
+////            }else if (pathSelection.toLowerCase().matches("return")) {
+//////                return "return";
+////            } else {
+////                System.out.println("Invalid input. Please try again.");
+////            }
+////        }while (pathSelection != "rent" || pathSelection != "return");
+////       return null;
+//    }
+//}
+
+
 public class HomeScreen {
     static public void printPathOptions(ArrayList<Car> carInventoryArr){
         System.out.println("We have "+carInventoryArr.size()+" cars in our lineup. \n"+ "Would you like to...");
         if(SplitCars.giveCarsArrByAvailability(carInventoryArr, true).size() > 0)
-            System.out.println("1) Rent ("+SplitCars.giveCarsArrByAvailability(carInventoryArr, true).size()+" Car(s) Available)");
+            System.out.println("\tRent ("+SplitCars.giveCarsArrByAvailability(carInventoryArr, true).size()+" Car(s) Available)");
         if(SplitCars.giveCarsArrByAvailability(carInventoryArr, false).size() > 0)
-            System.out.println("2) Return ("+SplitCars.giveCarsArrByAvailability(carInventoryArr, false).size()+" Car(s) Available)");
+            System.out.println("\tReturn ("+SplitCars.giveCarsArrByAvailability(carInventoryArr, false).size()+" Car(s) Available)");
         System.out.println("Select an option...");
     }
     static public String readPathSelection() {
+        Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("Selection: ");
-            Scanner scanner = new Scanner(System.in);
             String pathSelection = scanner.nextLine();
-            if (pathSelection.toLowerCase().matches("rent") || pathSelection == "1") { //1 doesn't work...
+            if (pathSelection.toLowerCase().matches("rent")) {
                 return "rent";
-            } else if (pathSelection.toLowerCase().matches("return") || pathSelection == "2") { //2 doesn't work...
+            }else if (pathSelection.toLowerCase().matches("return")) {
                 return "return";
             } else {
                 System.out.println("Invalid input. Please try again.");
-                //bug comes during this scenerio: first, invalid (e.g. 'm') then 'return'. but dont get correct print. instead asks for selection without the 'invalid input warning.' do return again and it's fine.
             }
         }
     }
